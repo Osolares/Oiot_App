7d92109567c1896e9a6aad85fcfefe3b0b6e7c36
Osolares <osolares1116503@gmail.com>
2021-09-06 23:35:24 -0600

    Origin Oiot App

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     <side-bar
       :background-color="sidebarBackground"
       short-title="GL"
-      title="IoTicos GL"
+      title="Oiot"
     >
       <template slot-scope="props" slot="links">
         <sidebar-item
